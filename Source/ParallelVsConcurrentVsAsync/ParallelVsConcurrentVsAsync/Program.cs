a3c1823b3367d92eae9662922d8cf40b1dcfd412
Gintaras <42067367+GintaraSt@users.noreply.github.com>
2023-06-12 23:41:37 +0300

    More execution examples

--- Source/ParallelVsConcurrentVsAsync/ParallelVsConcurrentVsAsync/Program.cs
+++ Source/ParallelVsConcurrentVsAsync/ParallelVsConcurrentVsAsync/Program.cs
@@ -11,42 +11,93 @@ public class Program
     {
         Stopwatch sw;
 
-        var httpClient = new HttpClient();
+
 
         /// ---------------------------
         /// ------ Synchronous -------
         /// ---------------------------
-        sw = Stopwatch.StartNew();
+        //sw = Stopwatch.StartNew();
 
-        SendRequest(0, httpClient);
-        SendRequest(1, httpClient);
-        SendRequest(2, httpClient);
-        SendRequest(3, httpClient);
+        //SendRequest(0);
+        //SendRequest(1);
+        //SendRequest(2);
+        //SendRequest(3);
 
-        sw.Stop();
-        Console.WriteLine($"Time took: {sw.ElapsedMilliseconds}ms, {sw.ElapsedTicks}ticks");
+        //sw.Stop();
+        //Console.WriteLine($"Time took: {sw.ElapsedMilliseconds}ms, {sw.ElapsedTicks}ticks");
 
         /// ---------------------------
         /// ------ Asynchronous -------
         /// ---------------------------
+        // // Force app to only use 1 CPU Core
+        // Process proc = Process.GetCurrentProcess();
+        // long affinityMask = 0x0001;
+        // proc.ProcessorAffinity = (IntPtr)affinityMask;
+        // sw = Stopwatch.StartNew();
+
+        // var asyncTask0 = SendRequestAsync(0);
+        // var asyncTask1 = SendRequestAsync(1);
+        // var asyncTask2 = SendRequestAsync(2);
+        // var asyncTask3 = SendRequestAsync(3);
+        // // DoWork(4); // This can be done while async tasks are running.
+
+        // await Task.WhenAll(asyncTask0, asyncTask1, asyncTask2, asyncTask3);
+        // sw.Stop();
+        // Console.WriteLine($"Time took: {sw.ElapsedMilliseconds}ms, {sw.ElapsedTicks}ticks");
+
+        /// ---------------------------
+        /// -------- Parallel ---------
+        /// ---------------------------
+        // sw = Stopwatch.StartNew();
+
+        // var parallelTask0 = Task.Run(() => SendRequest(0));
+        // var parallelTask1 = Task.Run(() => SendRequest(1));
+        // var parallelTask2 = Task.Run(() => SendRequest(2));
+        // var parallelTask3 = Task.Run(() => SendRequest(3));
+        // DoWork(4);
+
+        // await Task.WhenAll(parallelTask0, parallelTask1, parallelTask2, parallelTask3);
+        // sw.Stop();
+        // Console.WriteLine($"Time took: {sw.ElapsedMilliseconds}ms, {sw.ElapsedTicks}ticks");
+
+        /// --------------------------------------
+        /// -------- Parallel/Concurrent ---------
+        /// --------------------------------------
         sw = Stopwatch.StartNew();
 
-        //await WriteDataAsync(0, dataToWrite);
-        //await WriteDataAsync(1, dataToWrite);
-        //await WriteDataAsync(3, dataToWrite);
-        //await WriteDataAsync(3, dataToWrite);
-        var asyncTask0 = SendRequestAsync(0, httpClient);
-        var asyncTask1 = SendRequestAsync(1, httpClient);
-        var asyncTask2 = SendRequestAsync(2, httpClient);
-        var asyncTask3 = SendRequestAsync(3, httpClient);
+        var parallelTasks = new List<Task>();
+        for (int i = 0; i < 200; i++)
+        {
+            RunInParallel(i);
+        }
+        DoWork(201);
 
-        await Task.WhenAll(asyncTask0, asyncTask1, asyncTask2, asyncTask3);
+        await Task.WhenAll(parallelTasks);
+
+        void RunInParallel(int id) => parallelTasks.Add(Task.Run(() => SendRequest(id)));
         sw.Stop();
         Console.WriteLine($"Time took: {sw.ElapsedMilliseconds}ms, {sw.ElapsedTicks}ticks");
 
-        /// ---------------------------
-        /// ------ Synchronous --------
-        /// ---------------------------
+        /// --------------------------------------
+        /// ----------- Asynchronous -------------
+        /// --------------------------------------
+        // sw = Stopwatch.StartNew();
+
+        // var asynchronousTasks = new List<Task>();
+        // for (int i = 0; i < 1000; i++)
+        // {
+        //     asynchronousTasks.Add(SendRequestAsync(i));
+        // }
+        // DoWork(1001);
+
+        // await Task.WhenAll(asynchronousTasks);
+
+        // sw.Stop();
+        // Console.WriteLine($"Time took: {sw.ElapsedMilliseconds}ms, {sw.ElapsedTicks}ticks");
+
+        /// ------------------------------------
+        /// ------ Synchronous CPU-Bound--------
+        /// ------------------------------------
         //sw = Stopwatch.StartNew();
 
         //DoWork(1);
@@ -102,19 +153,21 @@ public class Program
         Console.WriteLine($"Completed {id} - {DateTime.UtcNow.TimeOfDay}");
     }
 
-    static void SendRequest(int id, HttpClient httpClient)
+    static void SendRequest(int id)
     {
         Console.WriteLine($"Starting {id}: {DateTime.UtcNow.TimeOfDay}");
 
+        var httpClient = new HttpClient();
         httpClient.Send(new HttpRequestMessage(HttpMethod.Get, "http://localhost:5201/slow-response"));
 
         Console.WriteLine($"Completed {id} - {DateTime.UtcNow.TimeOfDay}");
     }
 
-    static async Task SendRequestAsync(int id, HttpClient httpClient)
+    static async Task SendRequestAsync(int id)
     {
         Console.WriteLine($"Starting {id}: {DateTime.UtcNow.TimeOfDay}");
 
+        var httpClient = new HttpClient();
         await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, "http://localhost:5201/slow-response"));
 
         Console.WriteLine($"Completed {id} - {DateTime.UtcNow.TimeOfDay}");
